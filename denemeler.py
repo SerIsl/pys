202cee2899909f0df5a7cc317d223dd398aa677b
Serkan <serkanisler88@gmail.com>
2022-09-22 18:10:22 +0300

    cells fonksiyonu eklendi.

--- denemeler.py
+++ denemeler.py
@@ -1,37 +1,83 @@
-class Coordinate:
-    def __init__(self, x, y) -> None:
-        self.x = x
-        self.y = y
+import numpy
 
-    def __repr__(self) -> str:
-        return "Coord: " + str(self.__dict__)
+# class Coordinate:
+#     def __init__(self, x, y) -> None:
+#         self.x = x
+#         self.y = y
 
-def add(a, b):
-    return Coordinate(a.x + b.x, a.y + b.y)
+#     def __repr__(self) -> str:
+#         return "Coord: " + str(self.__dict__)
 
-def sub(a, b):
-    return Coordinate(a.x - b.x, a.y - b.y) 
-one = Coordinate(100, 200)
-two = Coordinate(300, 200)
-three = Coordinate(-100, -100)
+# def add(a, b):
+#     return Coordinate(a.x + b.x, a.y + b.y)
+
+# def sub(a, b):
+#     return Coordinate(a.x - b.x, a.y - b.y) 
+# one = Coordinate(100, 200)
+# two = Coordinate(300, 200)
+# three = Coordinate(-100, -100)
 
 
-def wrapper(func):
-    def checker(a, b):
-        if a.x < 0 or a.y < 0:
-            a = Coordinate(a.x if a.x > 0 else 0, a.y if a.y > 0 else 0)
+# def wrapper(func):
+#     def checker(a, b):
+#         if a.x < 0 or a.y < 0:
+#             a = Coordinate(a.x if a.x > 0 else 0, a.y if a.y > 0 else 0)
         
-        if b.x < 0 or b.y < 0:
-            b = Coordinate(b.x if b.x > 0 else 0, b.y if b.y > 0 else 0)
+#         if b.x < 0 or b.y < 0:
+#             b = Coordinate(b.x if b.x > 0 else 0, b.y if b.y > 0 else 0)
         
-        ret = func(a, b)
-        if ret.x < 0 or ret.y < 0:
-            ret = Coordinate(ret.x if ret.x > 0 else 0, ret.y if ret.y > 0 else 0)
-        return ret
-    return checker
+#         ret = func(a, b)
+#         if ret.x < 0 or ret.y < 0:
+#             ret = Coordinate(ret.x if ret.x > 0 else 0, ret.y if ret.y > 0 else 0)
+#         return ret
+#     return checker
 
-add = wrapper(add)
-sub = wrapper(sub)
+# add = wrapper(add)
+# sub = wrapper(sub)
 
-print(sub(one, two))
-print(add(one, three))
+# print(sub(one, two))
+# print(add(one, three))
+my_dict = {
+    'Malzeme Kodu': 'YP-01-015-001', 
+    'Malzeme Tanımı': 'KAZAN CERCEVE DKP 60X90', 
+    'Malzeme İng.Tanımı': 'X', 
+    'Miktar': 300.0, 
+    'Toplanan': 300.0, 
+    'Kalan': 0.0, 
+    'Sandık/Miktar': '44   /   43', 
+    'Sandık/Miktar.1': '45   /   43', 
+    'Sandık/Miktar.2': '46   /   43', 
+    'Sandık/Miktar.3': '47   /   43', 
+    'Sandık/Miktar.4': '48   /   43', 
+    'Sandık/Miktar.5': '49   /   43', 
+    'Sandık/Miktar.6': '50   /   42',
+    'Sandık/Miktar.7': nan, 
+    'Sandık/Miktar.8': nan, 
+    'Sandık/Miktar.9': nan
+    }
+def cells(satir):
+    malzeme_kodu = ""
+    mydict1 = []
+
+    for key, value in satir.items():
+        kasa_no = 0
+        adet = 0
+        tempdict = {}
+        if type(value) != str:
+            continue 
+
+        if key == "Malzeme Kodu":
+            malzeme_kodu = value
+        elif "Sandık/Miktar" in key:
+            
+            kasa_no = "M"+value.split("/")[0].replace(" ", "")
+            adet = value.split("/")[1].replace(" ", "")
+            tempdict["Kasa No"] = kasa_no
+            tempdict["malzeme_kodu"] = malzeme_kodu
+            tempdict["adet"] = adet
+            mydict1.append(tempdict)
+    
+    print(*mydict1, sep="\n")
+
+
+cells(my_dict)
--- nevinhn.py
+++ nevinhn.py
@@ -4,21 +4,33 @@ df = pd.read_excel("DemonteM20220000000372.xls")
 
 
 sozluk = df.to_dict("records")
-dict_3 = sozluk[133]
-print(dict_3)
-malzeme_kodu = ""
 
-for key, value in dict_3.items():
-    Kasa_No = 0
-    adet = 0
-    if type(value) == float:
-        continue
-    if key == "Malzeme Kodu":
-        malzeme_kodu = value
-    elif "Sandık/Miktar" in key:
-        Kasa_No = "M" + value.split("/")[0].replace(" ", "")
-        adet = value.split("/")[1].replace(" ", "")
-        print("Kasa no: " + Kasa_No + " Adet: " + adet + " Malzeme Kodu: "+ malzeme_kodu)
+dict_3 = sozluk[133]
+print(sozluk)
+
+def cells(satir):
+    malzeme_kodu = ""
+    mydict1 = []
     
+    for key, value in satir.items():
+        kasa_no = 0
+        adet = 0
+        tempdict = {}
+        if type(value) != str:
+            continue 
+
+        if key == "Malzeme Kodu":
+            malzeme_kodu = value
+        elif "Sandık/Miktar" in key:
+            
+            kasa_no = "M"+value.split("/")[0].replace(" ", "")
+            adet = value.split("/")[1].replace(" ", "")
+            tempdict["Kasa No"] = kasa_no
+            tempdict["malzeme_kodu"] = malzeme_kodu
+            tempdict["adet"] = adet
+            mydict1.append(tempdict)
+
+    print(*mydict1, sep="\n")
+
 
 
