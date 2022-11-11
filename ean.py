4ddfcb3cf54861496db27e51cd952c5d7d9b76d7
Serkan <serkanisler88@gmail.com>
2022-11-11 13:41:44 +0300

    ean fonksiyonuna eklemeler yapıldı.

--- ean.py
+++ ean.py
@@ -2,6 +2,15 @@ while True:
     ean = input("Sayı giriniz:\t")
     if ean.lower() == "ok":
         break
+    elif len(ean) < 12:
+        print(f"Girdiğiniz sayı en az 12 basamaklı olmalıdır. Girilen sayı {len(ean)} basamaklı.")
+        continue
+    elif len(ean) > 12:
+        print(f"Girdiğiniz sayı 12 basamaktan fazla. Girilen sayı {len(ean)} basamaklı.")
+        continue
+    elif not ean.isnumeric():
+        print("12 basamaklı bir sayı giriniz.")
+        continue
 
     tek = 0
     cift = 0
