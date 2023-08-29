f5b63dafb849af4f5b00087f9b4befc77920760b
Serkan <serkanisler88@gmail.com>
2023-08-29 16:35:40 +0300

    sirala güncellendi.

--- sirala.py
+++ sirala.py
@@ -1,8 +1,24 @@
+import pandas as pd
+
+def eklemeli(x):
+    barkod = input("barkodu giriniz: ")
+    
+    li = [barkod + f"{str(y):>04}" for y in x]
+
+    df = pd.DataFrame(li,columns=["Barkods"])
+    df.to_csv("Barkods.csv", sep=";", index=False)
+    print("Barkods.csv kullanıma hazır.")
+
 def sirala():
     all = set()
     same = {}
     entry = ""
+    to_csv = False
     mod = int(input("Lütfen Modu seçin (0/1):"))
+    if mod == 0:
+        secim = input("Csv çıktısı almak istiyor musunuz? E/H")
+        if secim.lower() == "e":
+            to_csv = True
     while True:
         print(f"\nGirilerin toplam sayısı: {len(all)}")
         entry = input('Bitirmek için ok yaz!\ngiriş yap:\t')
@@ -33,3 +49,6 @@ def sirala():
     print("Toplam kayıt sayısı:", len(list_all))
     print("Benzerler: ", same)
     print("Benzerlerin toplam sayısı: ", sum(list(same.values())))
+
+    if to_csv == True:
+        eklemeli(list_all)
